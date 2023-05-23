bc62cc987f3ec2b1a68d60b59f22cfb1783dc9c5
Marc Lütke <marc.luetke@icloud.com>
2023-05-23 18:36:47 +0200

    Fixing package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ThemePackage",
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,7 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ThemePackage",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Resources")
+//                .process("ThemeColors.xcassets")
+            ]),
         .testTarget(
             name: "ThemePackageTests",
             dependencies: ["ThemePackage"]),
